f300e14dac3866884ebdcbe6655b3d4f13904647
emrekardaslar <emre.kardaslar@gmail.com>
2022-08-28 14:25:01 +0300

    post data to firebase

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -48,7 +48,12 @@ const Checkout = (props) => {
       return;
     }
 
-    // Submit cart data
+    props.onConfirm({
+      name: enteredName,
+      street: enteredStreet,
+      city: enteredCityIsValid,
+      postalCode: enteredPostalCodeIsValid
+    });
   };
 
   const nameControlClasses = `${classes.control} ${
